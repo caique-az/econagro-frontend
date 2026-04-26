3e8e92cf5caf3aecfaac54a7e1686cb58d38e952
caique-az <caiqueazevedo2005@gmail.com>
2026-04-25 21:46:57 -0300

    refactor(category): migrate images to cloudinary

--- src/data/categories.jsx
+++ src/data/categories.jsx
@@ -1,19 +1,10 @@
-import imagem1 from '../assets/imagem1.jpg';
-import imagem2 from '../assets/imagem2.jpg';
-import imagem3 from '../assets/imagem3.jpg';
-import imagem4 from '../assets/imagem4.jpg';
-import imagem5 from '../assets/imagem5.jpg';
-import imagem6 from '../assets/imagem6.jpg';
-import imagem7 from '../assets/imagem7.jpg';
-import imagem8 from '../assets/imagem8.jpg';
-
 export const categories = [
-  { name: 'Carnes',     img: imagem1 },
-  { name: 'Ovos',       img: imagem2 },
-  { name: 'Laticínios', img: imagem3 },
-  { name: 'Grãos',      img: imagem4 },
-  { name: 'Frutas',     img: imagem5 },
-  { name: 'Verduras',   img: imagem6 },
-  { name: 'Legumes',    img: imagem7 },
-  { name: 'Sementes',   img: imagem8 },
+  { name: 'Carnes',     img: 'https://res.cloudinary.com/drmsacagg/image/upload/v1777161091/carnes.jpg' },
+  { name: 'Ovos',       img: 'https://res.cloudinary.com/drmsacagg/image/upload/v1777161092/ovos.jpg' },
+  { name: 'Laticínios', img: 'https://res.cloudinary.com/drmsacagg/image/upload/v1777161092/laticinios.jpg' },
+  { name: 'Grãos',      img: 'https://res.cloudinary.com/drmsacagg/image/upload/v1777161092/graos.jpg' },
+  { name: 'Frutas',     img: 'https://res.cloudinary.com/drmsacagg/image/upload/v1777161092/frutas.jpg' },
+  { name: 'Verduras',   img: 'https://res.cloudinary.com/drmsacagg/image/upload/v1777161093/verduras.jpg' },
+  { name: 'Legumes',    img: 'https://res.cloudinary.com/drmsacagg/image/upload/v1777161092/legumes.jpg' },
+  { name: 'Sementes',   img: 'https://res.cloudinary.com/drmsacagg/image/upload/v1777161092/sementes.jpg' },
 ];
