5a4ca76ee8e8e36f52f848b8d13fef0533566f9a
caique-az <caiqueazevedo2005@gmail.com>
2026-04-27 15:22:51 -0300

    refactor: remove deprecated code

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,4 +1,4 @@
-import '../app/globals.css';
+import './globals.css';
 import Header from '../components/header';
 import Footer from '../components/footer';
 import { Providers } from '../components/Providers';
