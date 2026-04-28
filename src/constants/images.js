55ca2e0e1d2e5b03dc1ee2568b5e9971b99cebaf
caique-az <caiqueazevedo2005@gmail.com>
2026-04-28 12:21:47 -0300

    feat: add FALLBACK_IMAGE_CATEGORY constant

--- src/constants/images.js
+++ src/constants/images.js
@@ -2,3 +2,5 @@ export const FALLBACK_IMAGE_PRODUCT =
   "https://placehold.co/300x200?text=Imagem+não+encontrada";
 export const FALLBACK_IMAGE_CART =
   "https://placehold.co/100x100?text=Sem+Imagem";
+export const FALLBACK_IMAGE_CATEGORY =
+  "https://placehold.co/400x200?text=Categoria";
