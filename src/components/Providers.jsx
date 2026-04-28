4d5fe13d77a4bfe506d5ae2e7e070af16c624366
caique-az <caiqueazevedo2005@gmail.com>
2026-04-28 11:35:11 -0300

    feat: wrap app with AuthProvider

--- src/components/Providers.jsx
+++ src/components/Providers.jsx
@@ -2,11 +2,14 @@
 
 import { CartProvider } from "../context/CartContext";
 import { SearchProvider } from "../context/SearchContext";
+import { AuthProvider } from "../context/AuthContext";
 
 export function Providers({ children }) {
   return (
-    <SearchProvider>
-      <CartProvider>{children}</CartProvider>
-    </SearchProvider>
+    <AuthProvider>
+      <SearchProvider>
+        <CartProvider>{children}</CartProvider>
+      </SearchProvider>
+    </AuthProvider>
   );
 }
